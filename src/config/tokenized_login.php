d6f1f27dcde592e2e29da32264b6ba55891df2de
imanghafoori <iman_github1@yahoo.com>
2020-02-16 00:32:09 +0330

    add doc blocks for config file

--- src/config/tokenized_login.php
+++ src/config/tokenized_login.php
@@ -6,17 +6,27 @@ return [
      */
     'token_ttl' => 120,
 
+    /**
+     * Here you determine if you are ok with using the routes
+     * defined within the package or you want to define them
+     */
     'use_default_routes' => true,
 
-    'route_middlewares' => ['api'],
     /**
-     * Route prefix for token stuff.
-     * Note: it should not end in a slash character.
+     * Here you can specify the middlewares to be applied on
+     * the routes, which the package has provided for you.
+     */
+    'route_middlewares' => ['api'],
+
+    /**
+     * You can define a prefix for the urls to avoid conflicts.
+     * Note: the prefix should NOT end in a slash / character.
      */
     'route_prefix_url' => '/tokenized-login',
 
     /**
-     * Notification class used to send the token to user.
+     * Notification class used to send the token.
+     * You may define your own token sender class
      */
     'token_sender' => \Imanghafoori\TokenizedLogin\TokenSender::class,
 ];
