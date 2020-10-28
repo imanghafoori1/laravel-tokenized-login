a80e81786cb2c7a5bc7d9332317a15f2e96ee10a
Reza Amini <rezaaminiroyal@gmail.com>
2020-10-28 17:57:33 +0330

    update route syntax with laravel 8

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,13 @@
 <?php
 
+use Imanghafoori\TokenizedLogin\Http\Controllers\TokenSenderController;
+
 Route::post(config('tokenized_login.route_prefix_url').'/request-token',
-    'TokenSenderController@issueToken')
+    [TokenSenderController::class, 'issueToken'])
     ->name('tokenized_login.requestToken')
     ->middleware(config('tokenized_login.throttler_middleware'));
 
 Route::post(config('tokenized_login.route_prefix_url').'/login',
-    'TokenSenderController@loginWithToken')
+    [TokenSenderController::class, 'loginWithToken'])
     ->name('tokenized_login.login')
     ->middleware(config('tokenized_login.throttler_middleware'));
